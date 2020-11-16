d67b4f3ae991aa841af37c9a209e912342add987
Andrew Taghi <aataghi13@gmail.com>
2020-11-16 15:47:52 -0600

    updated search function to require check-in and check-out date before returning any values

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -18,6 +18,7 @@ export class SearchComponent implements OnInit {
     private reservationService: ReservationService
     ) { }
 
+    // Access the reservations database to determine if there are conflicting dates. 
   ngOnInit() {
     this.reservationService.getReservations().subscribe((response) => {
       this.reservations = response;
@@ -27,25 +28,33 @@ export class SearchComponent implements OnInit {
 
   // The search function will run once the search button is clicked, and will access all of the data in the rooms DB. First, determine if the room is in the reservations table. If so, check the dates for that room. If the date range requested is outside of the date range in the reservation table, display that room. Else, do not display that room. 
   search() {
-    console.log(this.checkIn);
-    console.log(this.checkOut);
-    console.log(this.reservationService);
-    for (let i = 0; i < this.reservations.length; i++) {
-      console.log(this.reservations[i]);
-      console.log(this.reservations[i].check_in);
-      console.log(this.reservations[i].check_out);
-      if (this.checkOut < this.reservations[i].check_in || this.checkIn > this.reservations[i].check_out) {
-        console.log(`Date range works for room ${this.reservations[i].room_number}`);
-        i++;
-      } else if (this.reservations[i].check_in < this.checkIn && this.checkIn < this.reservations[i].check_out) {
-        console.log(`${this.checkIn} is within a current reservation and room ${this.reservations[i].room_number} is not available`);
-        i++;
+
+    if (this.checkIn && this.checkOut) {
+      this.roomService.getRooms().subscribe((response) => {
+        this.rooms = response;
+      })
+
+      for (let i = 0; i < this.reservations.length; i++) {
+        console.log(this.reservations[i]);
+        console.log(this.reservations[i].check_in);
+        console.log(this.reservations[i].check_out);
+        if (this.checkOut < this.reservations[i].check_in || this.checkIn > this.reservations[i].check_out) {
+          console.log(`Date range works for room ${this.reservations[i].room_number}`);
+          i++;
+        } else if (this.reservations[i].check_in < this.checkIn && this.checkIn < this.reservations[i].check_out) {
+          console.log(`${this.checkIn} is within a current reservation and room ${this.reservations[i].room_number} is not available`);
+          i++;
+        } else {
+          console.log(`Room ${this.reservations[i].room_number} is available for reservation on all dates`);
+          
+        }
       }
+
+    } else {
+      window.alert(`Check-In & Check-Out Required!`)
     }
     
-    this.roomService.getRooms().subscribe((response) => {
-      this.rooms = response;
-    })
+    
   }
 
   // When the user clicks on the reserve button, the room_number as well as the checkIn and checkOut data should be added to the reservation table
